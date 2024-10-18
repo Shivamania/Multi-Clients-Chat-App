dff2b4f8f4dfb90644c5df40911511b96b676dc6
Shivam Patel <141944585+Shivamania@users.noreply.github.com>
2024-10-18 12:50:06 +0530

    Add files via upload

---
+++ Multi-Clients Chat App/src/Server.java
@@ -0,0 +1,165 @@
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+
+public abstract class Server implements ActionListener, Runnable {
+
+    JTextField text;
+    JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+    static DataOutputStream dout;
+    static ServerSocket skt;
+
+    Server() {
+        initializeGUI();
+    }
+
+    public void initializeGUI() {
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        // Remaining GUI initialization code...
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70);
+        p1.setLayout(null);
+        f.add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50, 50);
+        p1.add(profile);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30, 30);
+        p1.add(video);
+
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(360, 20, 35, 30);
+        p1.add(phone);
+
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        JLabel morevert = new JLabel(i15);
+        morevert.setBounds(420, 20, 10, 25);
+        p1.add(morevert);
+
+        JLabel name = new JLabel("Gaitonde");
+        name.setBounds(110, 15, 100, 18);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        p1.add(name);
+
+        JLabel status = new JLabel("Active Now");
+        status.setBounds(110, 35, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        p1.add(status);
+
+        a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 570);
+        f.add(a1);
+
+        text = new JTextField();
+        text.setBounds(5, 655, 310, 40);
+        text.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(text);
+
+        JButton send = new JButton("Send");
+        send.setBounds(320, 655, 123, 40);
+        send.setBackground(new Color(7, 94, 84));
+        send.setForeground(Color.WHITE);
+        send.addActionListener(this);
+        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f.add(send);
+
+        f.setSize(450, 700);
+        f.setLocation(200, 50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+
+        f.setVisible(true);
+    }
+
+    Socket socket;
+
+    public static Vector client = new Vector();
+
+    public Server (Socket socket) {
+        try {
+            this.socket = socket;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            client.add(writer);
+
+            while(true) {
+                String data = reader.readLine().trim();
+                System.out.println("Received " + data);
+                for (int i = 0; i < client.size(); i++) {
+                    try {
+                        BufferedWriter bw = (BufferedWriter) client.get(i);
+                        bw.write(data);
+                        bw.write("\r\n");
+                        bw.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket s = new ServerSocket(2003);
+        while(true) {
+            Socket socket = s.accept();
+            Server server = new Server(socket) {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            };
+            Thread thread = new Thread(server);
+            thread.start();
+        }
+    }
+}
+
